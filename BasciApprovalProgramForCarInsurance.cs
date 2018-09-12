abf465564ad94d5efb830ead23cedf5aea5dc8b5
grund <grundy_j@live.com>
2018-09-12 09:32:37 -0700

    initial commit

---
+++ BasciApprovalProgramForCarInsurance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BasicApprovalProgramForCarInsurance
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Please answer Yes or No.");
+            string dui = Console.ReadLine();
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt16(Console.ReadLine());
+            bool eligable = age > 15;
+            bool risk = dui == "No";
+            bool violations = tickets < 4;
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(eligable && risk && violations);
+            Console.ReadLine();
+        }
+    }
+}
