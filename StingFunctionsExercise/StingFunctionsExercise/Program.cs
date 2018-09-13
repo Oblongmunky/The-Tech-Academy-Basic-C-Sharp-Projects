8e4721e54f342473394ec56bac5ff724fd02a09a
grund <grundy_j@live.com>
2018-09-13 14:48:58 -0700

    initial commit

---
+++ StingFunctionsExercise/StingFunctionsExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace StingFunctionsExercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = "Jeremy";
+            string hi = "Hi,";
+            StringBuilder sb = new StringBuilder("Hello!");
+            sb.Append(" My purpose is to greet you.");
+            sb.Append("\nI do this by typeing out a small paragraph to you.");
+            sb.Append("\nThis will be done in only a few lines. ");
+            sb.Append("\nIt was nice to meet you!");
+            hi = hi.ToUpper();
+            Console.WriteLine(hi + name + "\n" + sb);
+            Console.ReadLine();
+        }
+    }
+}
