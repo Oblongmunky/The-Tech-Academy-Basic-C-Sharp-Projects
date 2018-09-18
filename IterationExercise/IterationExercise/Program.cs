5edf1e5fda5d85d8db75fb04890d8199eb1bcd3b
grund <grundy_j@live.com>
2018-09-18 14:06:45 -0700

    initial commit

---
+++ IterationExercise/IterationExercise/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IterationExercise
+{
+    class Program
+    {
+        static void Main()
+        {   //1.
+            //string[] numbersAlpha = { "one", "two", "three", "four", "five" };
+            //Console.WriteLine("Please enter your name.");
+            //string name = Console.ReadLine();
+
+
+            //foreach (string nA in numbersAlpha)
+            //{
+            //    Console.WriteLine(nA + name); 
+            //}
+            //Console.ReadLine();
+
+            //2. and 3.
+            //string[] numbersAlpha = { "one", "two", "three", "four", "five" };
+            //Console.WriteLine("Please enter your name.");
+            //string name = Console.ReadLine();
+
+            //for (; ;)
+            //{
+            //    Console.WriteLine(numbersAlpha[0] + name);
+            //    Console.WriteLine(numbersAlpha[1] + name);
+            //    Console.WriteLine(numbersAlpha[2] + name);
+            //    Console.WriteLine(numbersAlpha[3] + name);
+            //    Console.WriteLine(numbersAlpha[4] + name);
+            //}
+
+
+            //4.
+            //int[] numbers = { 1, 2, 3, 4, 5 };
+            //for (int i = 0; i < numbers.Length; i++);
+
+            //5.
+            //int[] numbers = { 1, 2, 3, 4, 5 };
+            //for (int i = 0; i <= numbers.Length; i++);
+
+            //6. 7. 8.
+
+            //List<string> names = new List<string> { "Jeremy Grundy", "Andrea Brewer", "Harper Brewer Grundy" };
+            //Console.WriteLine("Please type in Jeremy, Andrea or Harper to search the list.");
+            //string answer = Console.ReadLine();
+
+
+            //if (answer == "Jeremy")
+            //    {
+            //        Console.WriteLine(names[0]);
+            //    }
+            //    else if (answer == "Andrea")
+            //    {
+            //        Console.WriteLine(names[1]);
+            //    }
+            //    else if (answer == "Harper")
+            //    {
+            //        Console.WriteLine(names[2]);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Not a valid input");
+            //    }    
+            //    Console.ReadLine();
+
+
+            //9. 10.
+            //List<string> names = new List<string> { "Jeremy Grundy", "Andrea Brewer", "Harper Brewer Grundy", "Jeremy Grundy" };
+            //Console.WriteLine("Please type in Jeremy, Andrea or Harper to search the list for index number.");
+            //string answer = Console.ReadLine();
+
+
+            //if (answer == "Jeremy")
+            //{
+            //    Console.WriteLine(0);
+            //}
+            //else if (answer == "Andrea")
+            //{
+            //    Console.WriteLine(1);
+            //}
+            //else if (answer == "Harper")
+            //{
+            //    Console.WriteLine(2);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not a valid input");
+            //}
+            //Console.ReadLine();
+
+            //11.
+            //List<string> names = new List<string> { "Jeremy Grundy", "Andrea Brewer", "Harper Brewer Grundy", "Jeremy Grundy" };
+            //Console.WriteLine("Please type in Jeremy to see how many times it appears in the list");
+            //string answer = Console.ReadLine();
+
+            //foreach (string name in names)
+            //{
+            //    if (answer == "Jeremy")
+            //    {
+            //        Console.WriteLine(answer + " appears 2 times");
+            //        break;
+            //    }
+            //}
+            //Console.ReadLine();
+
+
+        }
+    }
+}
