255d8447bf8e36593d79f51e46ffe0076739af04
grund <grundy_j@live.com>
2018-09-18 14:23:27 -0700

    initial commit

---
+++ ExceptionHandlingExercise/ExceptionHandlingExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            { 
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Pick a number to divide each number in the list by");
+            int numberOne = Convert.ToInt16(Console.ReadLine());
+                foreach (int number in numbers)
+                {
+                    Console.WriteLine(number / numberOne);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
