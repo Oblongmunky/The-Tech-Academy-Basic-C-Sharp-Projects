809bd0b1b439e1d3dec13f859354ca0adb7c865f
grund <grundy_j@live.com>
2018-09-19 16:21:11 -0700

    initial commit

---
+++ VoidMethodExercise/VoidMethodExercise/MyVoidMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodExercise
+{
+    public class MyVoidMethod
+    {
+        public void addTwo(int num1, int num2)
+        {
+            num1 = Convert.ToInt16(Console.ReadLine()) + 1;
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ VoidMethodExercise/VoidMethodExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyVoidMethod run = new MyVoidMethod();
+            Console.WriteLine("type a number");
+            int pick1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("type another number");
+            int pick2 = 4;
+            run.addTwo(num1: pick1, num2: pick2);
+
+            Console.ReadLine();
+        }
+        
+    }
+}
