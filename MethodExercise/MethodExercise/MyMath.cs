b39ff439b78120deeabfc908fa3792dcedcb0fc1
grund <grundy_j@live.com>
2018-09-19 14:08:11 -0700

    initial commit

---
+++ MethodExercise/MethodExercise/MyMath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise
+{
+    public class MyMath
+    {
+        public int AddTwoMinusOne(int num1)
+        {
+            return num1 + num1 - num1;
+        }
+
+        public int TimesFourDivideTwice(int num2)
+        {
+            return num2 * 4 / num2 / num2;
+        }
+
+        public int Square(int num3)
+        {
+            return num3 * num3;
+        }
+    }
+}
---
+++ MethodExercise/MethodExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath a1 = new MyMath();
+            Console.WriteLine("Please pick a number for me to do math with");
+            int pick = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(a1.AddTwoMinusOne(pick));
+            Console.WriteLine("Please pick another number for me to do math with");
+            int pick2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(a1.TimesFourDivideTwice(pick2));
+            Console.WriteLine("Please pick a third number for me to do math with");
+            int pick3 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(a1.Square(pick3));
+            Console.ReadLine();
+        }
+    }
+}
