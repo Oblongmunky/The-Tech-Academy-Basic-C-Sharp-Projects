14926085f59dd605ad1d8be16537b54dd0d9dc55
grund <grundy_j@live.com>
2018-09-20 16:42:30 -0700

    initial commit

---
+++ ThreeMethodExercise/ThreeMethodExercise/MyMath.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeMethodExercise
+{
+    public class MyMath
+    {
+        public int addTwo(int a)
+        {
+            return a + 2;
+        }
+        public decimal addTwo(decimal a)
+        {
+            return Convert.ToInt16(a + 3);
+        }
+        public string addTwo(string a)
+        {
+            a = "12";
+            int i = 0;
+            if (!Int32.TryParse(a, out i))
+            {
+                i = -1;
+            }
+            return a;
+        }
+    }
+}
---
+++ ThreeMethodExercise/ThreeMethodExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeMethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyMath a1 = new MyMath();
+            //Console.WriteLine(a1.addTwo(12));
+            //Console.ReadLine();
+
+            //MyMath a1 = new MyMath();
+            //Console.WriteLine(a1.addTwo(12.9m));
+            //Console.ReadLine();
+
+            MyMath a1 = new MyMath();
+            Console.WriteLine(a1.addTwo("12"));
+            Console.ReadLine();
+        }
+    }
+}
