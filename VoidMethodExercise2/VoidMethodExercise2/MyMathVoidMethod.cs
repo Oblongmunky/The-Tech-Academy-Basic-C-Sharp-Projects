30ad2dd3d69f3d05b540bf69e060d1b5628beeec
grund <grundy_j@live.com>
2018-09-20 16:20:25 -0700

    initial commit

---
+++ VoidMethodExercise2/VoidMethodExercise2/MyMathVoidMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodExercise2
+{
+    public class MyMathVoidMethod
+    {
+        public void voidMath(int a, int b)
+        {
+            a = 2 + 4;
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ VoidMethodExercise2/VoidMethodExercise2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMathVoidMethod a1 = new MyMathVoidMethod();
+            a1.voidMath(12, 19);
+            //a1.voidMath(a: 12, b: 6);
+            Console.ReadLine();
+        }
+    }
+}
