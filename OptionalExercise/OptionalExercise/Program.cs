d1cf5e97893d70dbccc2de971336fb0808c6a9e7
grund <grundy_j@live.com>
2018-09-20 18:56:47 -0700

    initial commit

---
+++ OptionalExercise/OptionalExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyOptional a1 = new MyOptional();
+            Console.WriteLine("Please enter a number");
+            int uN1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Now you may choose to enter a second number but it is not required you can just hi enter");
+            int uN2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(a1.Opt(uN1, b: uN2));
+            Console.ReadLine();
+        }
+    }
+}
