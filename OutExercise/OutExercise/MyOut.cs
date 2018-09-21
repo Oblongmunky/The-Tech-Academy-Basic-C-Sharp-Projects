6b1bfa5e852b4b14f4f3f73cd6b425b4209626b1
grund <grundy_j@live.com>
2018-09-20 19:22:26 -0700

    initial commit

---
+++ OutExercise/OutExercise/MyOut.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutExercise
+{
+    public class MyOut
+    {
+        public void outPut(int a)
+        {
+            a = a / 2;
+        }
+        //public void outPut(decimal a)
+        //{
+        //    a = a / 2;
+        //}
+        //public void outPut(out int a)
+        //{
+        //    a = a / 2;
+        //}
+    }
+}
---
+++ OutExercise/OutExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyOut a1 = new MyOut();
+            Console.WriteLine("type a number");
+            int n1 = Convert.ToInt16(Console.ReadLine());
+            a1.outPut(n1);
+        }
+    }
+}
