6f9b8d6c1e673e5b371d32775e0a3b53631279b9
grund <grundy_j@live.com>
2018-09-24 11:55:41 -0700

    initial commit

---
+++ InterfaceExercise/InterfaceExercise/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExercise
+{
+    public class Employee : IQuittable
+    {
+        public void Quit()
+        {
+            Console.WriteLine("I Quit!");
+        }
+    }
+}
