bb2f3c94f7defb4ec1fb44918b2e0721a6d38192
grund <grundy_j@live.com>
2018-09-25 13:54:13 -0700

    initial commit

---
+++ EnumExercise/EnumExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExercise
+{
+    class Program
+    {
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a day of the week:");
+            bool exists;
+            exists = Enum.IsDefined(typeof(DaysOfTheWeek), Console.ReadLine());
+            Console.WriteLine(exists);
+            if (exists == false)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
