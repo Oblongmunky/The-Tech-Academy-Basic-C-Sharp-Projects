55d5f7be787a85411e66c800e0e0647ecd05c28e
grund <grundy_j@live.com>
2018-09-25 14:38:37 -0700

    initial commit

---
+++ StructsExercise/StructsExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number assigned = new Number();
+            assigned.Amount = 12.5m;
+            Console.WriteLine(assigned.Amount);
+            Console.ReadLine();
+        }
+    }
+}
