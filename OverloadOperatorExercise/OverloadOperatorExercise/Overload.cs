7d6b14967d9e33a1d09e06c9506c7778ce150457
grund <grundy_j@live.com>
2018-09-24 17:43:09 -0700

    initial commit

---
+++ OverloadOperatorExercise/OverloadOperatorExercise/Overload.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperatorExercise
+{
+    public class Number: Employee
+    {
+        int n;
+        public Number()
+        {
+            n = 0;
+        }
+        public Number(int x)
+        {
+            n = x;
+        }
+        public static bool operator ==(Number x, Number y)
+        {
+            if (x.n == y.n)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator !=(Number x, Number y)
+        {
+            if (x.n != y.n)
+                return true;
+            else
+                return false;
+        }
+        public int getValue()
+        {
+            return n;
+        }
+    }
+}
