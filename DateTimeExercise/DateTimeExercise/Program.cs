a57eac7eac62fba9a4bb8edc47cc0bfa7db25a8b
grund <grundy_j@live.com>
2018-09-30 16:41:24 -0700

    initial commit

---
+++ DateTimeExercise/DateTimeExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LogExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+            Console.WriteLine("The current Date\\Time is " + date);
+            Console.WriteLine("Please enter a number.");
+            double number = Convert.ToDouble(Console.ReadLine());
+            DateTime futureTime = date.AddHours(number);
+            Console.WriteLine(futureTime + " is what it will be in " + number + " hours.");
+            Console.ReadLine();
+        }
+    }
+}
