8db36372c9b37532134b6fa8e97b1cb3ac015265
grund <grundy_j@live.com>
2018-09-30 16:10:01 -0700

    initial commit

---
+++ LogExercise/LogExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LogExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            string number = string.Format(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\users\grund\logs\log.txt", true))
+            {
+                file.WriteLine(number);
+            }
+            Console.WriteLine(@"C:\users\grund\logs\log.txt");
+            Console.ReadLine();
+        }
+    }
+}
