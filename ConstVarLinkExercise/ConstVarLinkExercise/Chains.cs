7bce5dc31ee7e9eaa73f90bd19264d474a9ac2a7
grund <grundy_j@live.com>
2018-10-01 15:19:33 -0700

    inital commit

---
+++ ConstVarLinkExercise/ConstVarLinkExercise/Chains.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVarLinkExercise
+{
+    public class Chains
+    {
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public Chains(string firstName) : this(firstName, "n/a")
+        {
+        }
+        public Chains(string firstName, string lastName)
+        {
+            fName = firstName;
+            lName = lastName;
+        }
+    }
+}
---
+++ ConstVarLinkExercise/ConstVarLinkExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVarLinkExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string greeting = "Hello There Partner!";
+            Console.WriteLine(greeting + " Please type first name.");
+            var fName = Console.ReadLine();
+            Console.WriteLine("Now please enter your last name.");
+            string lName = Console.ReadLine();
+            Chains getName = new Chains(fName, lName);
+            Console.WriteLine(value: getName.fName + getName.lName);
+            Console.ReadLine();
+        }
+    }
+}
