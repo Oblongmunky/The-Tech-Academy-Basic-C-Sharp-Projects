3d886639d7b7af90b77c2c92fcd787449550d9c4
grund <grundy_j@live.com>
2018-10-02 14:57:29 -0700

    initial commit

---
+++ ExceptionHandlingExercise2/ExceptionHandlingExercise2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type your age.");
+            var currentYear = 2018;
+            try
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+                var yearBorn = currentYear - age;
+                if (age <= 0)
+                {
+                    Console.WriteLine("0 or negative numbers will not work.");
+                    Console.ReadLine();
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine(yearBorn);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Input age in digits only, with no decimal.");
+                Console.ReadLine();
+                return;
+            }
+            Console.ReadLine();
+        }
+    }
+}
